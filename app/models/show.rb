a32a18a02ff40644824c33dc010d68e1caa9f07c
Anil Gurung <anil_gurung15@yahoo.com>
2020-04-09 19:59:09 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+  has_many :characters 
+  has_many :actors, through: :characters
+  belongs_to :network
+  
+  def actors_list 
+    arr=[]
+    self.actors.each do |each|
+      arr<< "#{each.first_name} #{each.last_name}"
+    end
+    arr
+  end 
   
 end
